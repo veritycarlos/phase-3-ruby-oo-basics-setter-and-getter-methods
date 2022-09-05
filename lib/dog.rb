d4f72f6f1e3f8f0932ade4609b588aac925e80a3
Verity Collins <verity.carlos@gmail.com>
2022-09-05 15:10:38 -0500

    commit tree 8d69029739c8d7f4b4667fcd5c8dd8b59b660e3c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    #setter
+    def name=(name)
+        @name = name
+    end
+
+    #getter
+    def name
+        @name
+    end 
+
+    #setter
+    def breed=(breed)
+        @breed=breed
+    end
+
+    #getter
+    def breed
+        @breed
+    end
+    
 end
